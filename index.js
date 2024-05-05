ed6873f4b8898997fa6754df896ded79cf5c3ab8
EL Marsho <emarsho@gmail.com>
2024-05-04 22:38:54 -0500

    add header add responsiveness

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const menuIcon = document.getElementById("menu-icon");
-const menu = document.getElementById("menu");
 
-menuIcon.addEventListener("click", () => {
-    menu.className === "hidden" ? menu.classList.remove("hidden")
-        : menu.classList.add("hidden");
-})
 
 
 
