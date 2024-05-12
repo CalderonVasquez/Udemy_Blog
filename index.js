3944e047bacb2ebc2e4b9b3befd266813874fbe5
EL Marsho <emarsho@gmail.com>
2024-05-11 21:43:35 -0500

    add menu-icon add toggle

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
 
-
-
-
-
-
-
-
-
-
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
